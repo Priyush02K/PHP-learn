bec453ff829160d31eb2580a14b3710467d47399
Priyush02K <collegeinfopk02@gmail.com>
2025-04-19 15:39:09 +0530

    comment

---
+++ Comments/Comments.php
@@ -0,0 +1,19 @@
+<?php
+
+    #single line
+    //single line
+
+    echo "we leran comment <br>";
+
+
+
+    /* This is a multi line comment
+   In PHP variables are written
+   by adding a $ sign at the 
+   beginning.*/
+
+
+   $name= "priyush <br>";
+   echo $name;
+
+?>
