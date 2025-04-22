655f94fd1c0c608cbb3385315459d77da167bfb2
Priyush02K <collegeinfopk02@gmail.com>
2025-04-22 12:18:23 +0530

    echo and print

---
+++ PHP echo and print/echo.php
@@ -0,0 +1,22 @@
+<?php 
+
+// without parentheses:
+        echo "Hello, World!   <br>";
+
+
+// with parentheses
+        echo("Hello, World! <br>");
+
+    $name ="Hello Priyush";
+
+    $num1= 10;
+
+    $num2= 20;
+
+
+
+    echo $name . "\n";
+    echo $num1 . "+" . $num2 . "=";
+    echo $num1 +$num2;
+
+?>
---
+++ PHP echo and print/print.php
@@ -0,0 +1,17 @@
+<?php 
+    print "Hello, World! <br>";
+
+    print("Hello, World! <br>");
+
+
+    $text = "Hello, World! <br>";
+    $num1 = 10;
+    $num2 = 20;
+
+    // Print the value of variables
+    print $text."\n";
+    print $num1."+".$num2."=";
+    print $num1 + $num2;
+
+
+?>
